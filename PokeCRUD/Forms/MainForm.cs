c93ce501ea8779fbff618ae72fbe4701538ac6b5
AliA-Mohamad <ali.mohamad.uc@gmail.com>
2023-09-01 03:23:40 -0300

    teste com banco de dados

--- PokeCRUD/Forms/MainForm.cs
+++ PokeCRUD/Forms/MainForm.cs
@@ -14,6 +14,7 @@ namespace PokeCRUD
         {
             PokeData pokemonData = await PokeAPIService.GetRandomPokemon();
             label2.Text = pokemonData.Name;
+            testeSQL.inicia();
         }
     }
 }
---
+++ PokeCRUD/Services/testeSQL.cs
@@ -0,0 +1,29 @@
+﻿using System.Configuration;
+using System.Data.SqlClient;
+using Npgsql;
+
+namespace PokeCRUD.Services
+{
+    internal class testeSQL
+    {
+        public static void inicia()
+        {
+            string connectionString = "Server=motty.db.elephantsql.com;Database=fqsjuokr;User Id=fqsjuokr;Password=2CziZP6HbfASrTYOn_61MUCyTh_B6aqA;";
+            using (NpgsqlConnection connection = new NpgsqlConnection(connectionString))
+            {
+                connection.Open();
+
+                int idPokemon = 123;
+                string nomePokemon = "Bulbasaur";
+                string insertQuery = "INSERT INTO pokedex (Nome) VALUES (@Nome)";
+
+                using (NpgsqlCommand command = new NpgsqlCommand(insertQuery, connection))
+                {
+                    command.Parameters.AddWithValue("Nome", nomePokemon);
+
+                    int rowsAffected = command.ExecuteNonQuery();
+                }
+            }
+        }
+    }
+}
